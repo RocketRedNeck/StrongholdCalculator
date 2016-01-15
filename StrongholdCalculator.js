cf27fa3901ba7dd1cc4c2ef6e4ccefb91a8991b2
KidQuid <stundt1@gmail.com>
2016-01-15 12:14:39 -0500

    Basic Functions Working Need to fix GUI and add other modes

--- StrongholdCalculator.js
+++ StrongholdCalculator.js
@@ -1,19 +1,67 @@
+Autonomous = new Mongo.Collection(null);
+Teleop = new Mongo.Collection(null);
+Endgame = new Mongo.Collection(null);
+//init the arrays
+var autos = [
+  { name: 'Reaching a Defense', score: 0, multi: 2},
+  { name: 'Crossing a Defense', score: 0, multi:10},
+  { name: 'Boulder in a Low Tower Goal', score: 0 , multi:5},
+  { name: 'Bouder in a High Tower Goal', score: 0, multi:10}
+];
+var teleop = [
+  { name: 'Crossing a Defense', score: 0, multi: 5},
+  { name: 'Boulder in a low Tower Goal', score: 0, multi:2},
+  { name: 'Boulder in a High Tower Goal', score: 0 , multi:5}
+
+];
+
+var endgame = [
+  { name: 'Challenging the Tower', score: 0, multi: 5},
+  { name: 'Scailing the Tower', score: 0, multi:15}
+
+];
+
+for (var i = 0; i < autos.length; i++) {
+ Autonomous.insert(autos[i]);
+}
+for (var i = 0; i < teleop.length; i++) {
+ Teleop.insert(teleop[i]);
+}
+for (var i = 0; i < endgame.length; i++) {
+ Autonomous.insert(endgame[i]);
+}
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+  Session.setDefault('totalScore', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+    auto: function () {
+      return Autonomous.find({});
+    },
+    totalScore: function(){
+      return "Score: "+Session.get('totalScore');
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.item.events({
+    'click .add': function () {
+      var id=this._id;
+      var temp=Autonomous.find({_id : id},{fields:{_id:0,name:0,score:0}}).fetch();
+
+      Session.set('totalScore', Session.get('totalScore') + temp[0].multi);
+
+      Autonomous.update(this._id,{$inc: {score: 1} })
+    },
+    'click .sub': function () {
+      var id=this._id;
+      var temp=Autonomous.find({_id : id},{fields:{_id:0,name:0}}).fetch();
+      if (temp[0].score>0) {
+          Session.set('totalScore', Session.get('totalScore') - temp[0].multi);
+            Autonomous.update(this._id,{$inc: {score: -1} })
+      }
     }
   });
+
 }
 
 if (Meteor.isServer) {
